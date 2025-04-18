0ce5a0361e35dd72f3b23fc0b0f18aba3e49bd51
Teng Xing Tan <TANT441323@exactsoftware.com>
2025-04-18 15:14:27 +0800

    changeDetection OnPush (CheckOnce) vs Default (CheckAlways).

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@angular/core';
 import { HousingService } from '../housing.service';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { NgFor, SlicePipe } from '@angular/common';
@@ -9,17 +9,32 @@ import { SortByNamePipe } from '../pipes/sort-by-name.pipe';
   selector: 'app-home',
   imports: [NgFor, HousingLocationComponent, SlicePipe, SortByNamePipe],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
+  maxItemsToShow = 1;
   housingLocationList: HousingLocation[] = [];
   filteredLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
-  constructor() {
+
+  constructor(private ref: ChangeDetectorRef) {
+    this.showHousingLocationEvery3secs();
     this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
       this.housingLocationList = housingLocationList;
       this.filteredLocationList = housingLocationList;
     });
+  }
+
+  showHousingLocationEvery3secs() {
+    const intervalId = setInterval(() => {
+      if (this.maxItemsToShow >= 10) {
+        clearInterval(intervalId);
+        return;
+      }
+      this.maxItemsToShow++;
+      this.ref.detectChanges();
+    }, 300);
   }
 
   filterResult(text: string) {
